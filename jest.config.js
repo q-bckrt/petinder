2dccc6a74264c417498c10306dd6ac7a50e5924e
q-bckrt <pom.schc@egmail.com>
2025-05-21 11:07:01 +0200

    Set the title

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  // globalSetup: 'jest-preset-angular/global-setup',
+  moduleNameMapper: {
+    'src/(.*)': '<rootDir>/src/$1',
+  }
+};
